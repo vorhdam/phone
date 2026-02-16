948b5daddf57863051df2d104311292b10199200
Ádám Horváth <vorhdam@gmail.com>
2026-02-16 14:36:55 +0100

    Updated showcase page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Phone } from "@/registry/new-york/phone/ui/phone";
+import Link from "next/link";
 import * as React from "react";
 
 export default function Page() {
@@ -9,16 +10,29 @@ export default function Page() {
   });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
+    const { value } = e.target;
     setFormData((prev) => ({
       ...prev,
-      [name]: value,
+      phone: value,
     }));
-    console.log(value);
   };
 
   return (
-    <div className="flex items-center justify-center p-4 w-full">
+    <div className="flex flex-col items-center justify-center p-4 w-full gap-8">
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h1 className="text-2xl font-bold uppercase">
+          Shadcn Phone Input Registry
+        </h1>
+        <p>
+          See full documentation at here:{" "}
+          <Link
+            className="underline font-bold"
+            href={"https://github.com/vorhdam/phone"}
+          >
+            Github
+          </Link>
+        </p>
+      </div>
       <div className="w-80">
         <Phone
           name="phone"
@@ -26,6 +40,10 @@ export default function Page() {
           value={formData.phone}
           placeholder="Your phone number"
         />
+      </div>
+      <div className="flex flex-row items-center gap-1.5">
+        <span>Your phone number is:</span>
+        <code className="bg-card p-1 rounded-md">{formData.phone}</code>
       </div>
     </div>
   );
